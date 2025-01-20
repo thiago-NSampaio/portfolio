da23a6c371d05773880985669773473a908e99b9
Thiago Nascimento <tiago349132@gmail.com>
2025-01-20 10:48:14 -0400

    fetching certificates

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -1,9 +1,25 @@
-import { certificates } from "../utils/certifcates";
 import { Row } from "./Row";
 import { SwiperSlide } from "swiper/react";
 import { ArrowUpRight } from "lucide-react";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+import { Certificate } from "../types/Certificate";
+
 
 export function Certificates() {
+  const [certificates, setCertificates] = useState<Certificate[]>([]);
+
+  useEffect(() => {
+    api
+      .get("/certificates")
+      .then((res) => {
+        setCertificates(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching certificates:", error);
+      });
+  }, []);
+
   return (
     <Row sb={30}>
       {certificates.map((certificate) => (
@@ -13,28 +29,27 @@ export function Certificates() {
         >
           <div className="h-72 flex z-10">
             <img
-              src={certificate.src}
+              src={certificate.img}
               alt={certificate.name}
               className="object-cover p-2 w-full h-full object-top rounded-xl"
               draggable={false}
             />
           </div>
           <div className="invisible absolute inset-0 bg-gradient-to-tr bg-zinc-800  flex flex-col justify-center rounded-lg items-center z-10 scale-0 transition duration-300 group-hover:scale-100 group-hover:opacity-100 group-hover:visible">
-            <span className="text-zinc-300 pt-2 text-lg font-bold">
-              NLW Pocket Full Stack - Intermediário
+            <span className="text-teal-500 pt-2 text-md font-bold">
+              {certificate.name}
             </span>
             <span className="text-zinc-300 pt-2 text-lg font-semibold">
               Instituição
             </span>
-            <p className="text-white">Rocketseat</p>
+            <p className="text-white">{certificate.institute}</p>
             <span className="text-zinc-300 pt-2 text-lg font-semibold">
               Ano de conclusão
             </span>
-            <p className="text-white">2024</p>
-
+            <p className="text-white">{certificate.year}</p>
             <a
               className="flex pt-10 text-white font-semibold text-lg hover:text-teal-400"
-              href="https://alura.com"
+              href={certificate.link}
               target="_blank"
             >
               Ver Certificado
---
+++ src/types/Certificate.ts
@@ -0,0 +1,8 @@
+export interface Certificate{
+    id: string
+    name: string
+    img: string
+    link: string
+    year: string
+    institute: string
+}
