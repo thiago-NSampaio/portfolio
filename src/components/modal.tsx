214007c0088668c3110fbb812f29cf1c5a21f163
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2025-06-14 22:06:56 -0400

    changes for deploy

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -15,13 +15,13 @@ export function Modal({
   children,
 }: ModalProps) {
   return (
-    <div className="fixed inset-0 bg-black/60 flex items-center justify-center overflow-auto">
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center overflow-auto z-10">
       <div className="rounded-xl py-5 px-6 shadow-shape bg-zinc-900 w-11/12 md:w-1/2 space-y-5">
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <h2 className="text-lg font-semibold">{titleModal}</h2>
             <button type="button" onClick={closeModal}>
-              <X className="size-5 text-zinc-400" />
+              <X className="size-6 text-zinc-400" />
             </button>
           </div>
           {typeof subtitleModal === "string" ? (
