12541c45bee0954156af962fe50581bddfa37354
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2025-07-06 17:49:10 -0400

    fix bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Logo } from "./Logo";
 import { NavBar } from "./NavBar";
 import { useMediaQuery } from "react-responsive";
